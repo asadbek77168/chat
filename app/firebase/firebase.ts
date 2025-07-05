45e555a4dfe54c2eb4e2c84a1651f7d941148957
asadbek77168 <cayzosmmhelp@gmail.com>
2025-07-05 09:43:27 +0500

    Update firebase.ts

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -1,18 +1,25 @@
-import { initializeApp, getApp, getApps } from "firebase/app";
+// lib/firebase.ts
+
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Firebase konfiguratsiyasi
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDHQpalOp7JpeiiPaIggfvnvtKwhodwooE",
+  authDomain: "mustaqil-talim-ai.firebaseapp.com",
+  projectId: "mustaqil-talim-ai",
+  storageBucket: "mustaqil-talim-ai.firebasestorage.app",
+  messagingSenderId: "1070375034065",
+  appId: "1:1070375034065:web:15f7e8e04fe84ffe88f9f0",
 };
 
-// Initialize Firebase
+// Appni faqat bir marta initialize qilish
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Firebase servislarini olish
+const auth = getAuth(app);
 const firestore = getFirestore(app);
 
-// export
-export { firestore };
+// Tashqi fayllarga eksport
+export { auth, firestore };
